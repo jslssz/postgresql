51b342c9c0cdd9da8fc2a67162d859461a0e72e1
jxustcmh <jxrjcmh@163.com>
2019-04-21 11:36:47 +0800

    热启动，CURD,druid数据源

--- src/main/java/com/hx/postgresql/controller/CURDController.java
+++ src/main/java/com/hx/postgresql/controller/CURDController.java
@@ -18,11 +18,49 @@ public class CURDController {
     @Autowired
     private UsersMapper mapper;
 
+    @RequestMapping("/psql/update")
+    @ResponseBody
+    public Object update() {
+        Users user=new Users();
+        user.setId(10);
+        user.setScore(39.9F);
+        int res = mapper.updateByPrimaryKeySelective(user);
+        return res == 1 ? "success" : "failure";
+    }
+
+
+
+    @RequestMapping("/psql/del")
+    @ResponseBody
+    public Object del() {
+        int res = mapper.deleteByPrimaryKey(11);
+        return res == 1 ? "success" : "failure";
+    }
+
+
+
     @RequestMapping("/psql/get")
     @ResponseBody
     public Object getAll(){
         List<Users> list=mapper.getAll();
         return list;
+    }
+
+    @RequestMapping("/psql/insert")
+    @ResponseBody
+    public Object insert(){
+        Users users=new Users();
+        users.setId(13);
+        users.setTeam("火箭");
+        users.setPlayer("姚明");
+        users.setScore(39.0f);
+        try {
+            mapper.insert(users);
+            return "success";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "failure";
+        }
     }
 
 }
